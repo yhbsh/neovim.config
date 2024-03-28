4ed8596072b76599c6dc4f740f7465981b6d8f8b
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-03-28 15:14:45 +0300

    add dart lsp

--- init.lua
+++ init.lua
@@ -587,12 +587,14 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
+            local config = require 'lspconfig'
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for tsserver)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            config[server_name].setup(server)
+            config.dartls.setup {}
           end,
         },
       }

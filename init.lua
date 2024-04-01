574f979c6819b958833777c20cc10aaaf09a3433
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-04-01 20:23:56 +0300

    remove dartls and white lines

--- init.lua
+++ init.lua
@@ -150,23 +150,14 @@ lazy.setup({
           end
 
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
@@ -190,7 +181,6 @@ lazy.setup({
       local servers = {
         clangd = {},
         gopls = {},
-        dartls = {},
         lua_ls = {
           settings = {
             Lua = {

0cbc4cc0d6bca4340fcb80f4fde9e12d3ab226d5
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-06-09 05:46:56 +0300

    remove .stylua.toml and update init.lua

--- init.lua
+++ init.lua
@@ -1,28 +1,48 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
-vim.g.loaded_perl_provider = 0
-vim.opt.number = true
+-- Leader keys
+vim.g.mapleader        = ' '
+vim.g.maplocalleader   = ' '
+
+-- Providers
+vim.g.have_nerd_font   = true
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider  = 0
+
+-- Appearance settings
+vim.opt.number         = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.hlsearch = true
-vim.opt.guicursor = 'n-v-c-sm-r-cr-o-i-ci-ve:hor25'
+vim.opt.mouse          = 'a'
+vim.opt.showmode       = false
+vim.opt.clipboard      = 'unnamedplus'
+vim.opt.breakindent    = true
+vim.opt.signcolumn     = 'yes'
+vim.opt.list           = true
+vim.opt.listchars      = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand     = 'split'
+vim.opt.cursorline     = true
+vim.opt.guicursor      = 'n-v-c-sm-r-cr-o-i-ci-ve:hor25'
+
+-- File handling
+vim.opt.undofile       = true
+
+-- Search settings
+vim.opt.ignorecase     = true
+vim.opt.smartcase      = true
+vim.opt.hlsearch       = true
+
+-- UI behavior
+vim.opt.updatetime     = 250
+vim.opt.timeoutlen     = 300
+vim.opt.splitright     = true
+vim.opt.splitbelow     = true
+vim.opt.scrolloff      = 20
+
+-- Indentation settings
+vim.opt.tabstop        = 4
+vim.opt.shiftwidth     = 4
+vim.opt.softtabstop    = 4
+vim.opt.expandtab      = true
+
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })

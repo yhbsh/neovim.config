cca8cb26f6f53ad795e3579907267b1564b9fb4d
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-07-19 09:39:50 +0100

    remove command line stuff

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,49 +1,27 @@
 return {
     'hrsh7th/nvim-cmp',
-    event = {'InsertEnter', 'CmdlineEnter'},
+    event = 'InsertEnter',
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
     },
     config = function()
-        local cmp = require 'cmp'
+        local cmp = require('cmp')
 
         cmp.setup {
-            completion = {
-                completeopt = 'menu,menuone,noinsert',
-                keyword_length = 1
-            },
-            mapping = cmp.mapping.preset.insert {
+            mapping = cmp.mapping.preset.insert({
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
                 ['<CR>'] = cmp.mapping.confirm {
                     behavior = cmp.ConfirmBehavior.Replace,
-                    select = true
+                    select = true,
                 },
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-            },
+            }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'buffer' },
-                { name = 'path' }
+                { name = 'path' },
             }),
             formatting = {
                 format = function(entry, vim_item)
@@ -56,21 +34,5 @@ return {
                 end
             },
         }
-
-        cmp.setup.cmdline({ '/', '?' }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = 'buffer' }
-            }
-        })
-
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                { name = 'cmdline' }
-            })
-        })
     end,
 }
